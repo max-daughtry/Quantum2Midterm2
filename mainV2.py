9f150691493dd490838d7a87a772a73853382f9c
max-daughtry <maxdaughtry@knights.ucf.edu>
2024-04-16 19:43:13 -0400

    Created V2
    V2 is hella faster cause I did it way better now

--- mainV2.py
+++ mainV2.py
@@ -16,10 +16,12 @@ import matplotlib.pyplot as plt
 eps = 0.0000001
 
 # Load in the file
-data = np.loadtxt('Li1Pi_u-potential.dat')
-plt.plot(data[:,0],data[:,1])
-plt.show()
-exit()
+data = np.loadtxt()
+# l = 1100
+# u = 4000
+# plt.scatter(data[l:u,0], data[l:u,1])
+# plt.show()
+# exit()
 # Select the data for the x and V
 x = data[:,0]
 V = data[:,1]
@@ -33,6 +35,7 @@ min_i = int(np.where(V == min(V))[0])
 min_x = x[min_i]
 # The actual minimum energy value
 min_V = V[min_i]
+
 
 # List of the energies for each energy level (the answers)
 E = []
@@ -60,7 +63,6 @@ for n in range(5):
 
     # Loop until it converges (or until a certain max number of iterations)
     while abs(res - target) > eps:
-
         V_test = (V_test_max + V_test_min) / 2
 
         # Ok so this is where the weirdness really starts.
@@ -68,32 +70,65 @@ for n in range(5):
         # I did this by starting at the index of the minimum energy value,
         # then moving to the left until a potential value is found that is closest to V_test.
         # Then I take that best possible index (best_left_i) and use the corresponding x-value as the lower bound of the integration
-        i = min_i
-        best_left_i = i
-        delta_min = abs(V[0] - min_V)
-        while i >= 0:
-            delta = abs(V_test - V[i])
-            if delta < delta_min:
-                delta_min = delta
-                best_left_i = i
-            i -= 1
+        # i = min_i
+        # best_left_i = i
+        # delta_min = abs(V[0] - min_V)
+        # while i >= 0:
+        #     delta = abs(V_test - V[i])
+        #     if delta < delta_min:
+        #         delta_min = delta
+        #         best_left_i = i
+        #     i -= 1
+
+        # I basically just changed the index finder to do a binary search
+        # instead of run through the entire length of the array (which obviously makes it like a gagillion times faster)
+        min_guess_i = 0
+        max_guess_i = min_i
+        best_left_i = int((min_guess_i + max_guess_i) / 2)
+
+        index_finder_iteration = 0
+        while abs(V[best_left_i] - V_test) > eps:
+            best_left_i = int((min_guess_i + max_guess_i) / 2)
+            if V[best_left_i] < V_test:
+                max_guess_i = best_left_i
+            else:
+                min_guess_i = best_left_i
+            if index_finder_iteration > 1000:
+                break
+            index_finder_iteration+=1
 
         # everything I said above except now it's to the right and it's the upper bound of the integration
-        i = min_i
-        best_right_i = i
-        delta_min = abs(V[0] - min_V)
-        while i <= np.where(V == max(V[min_i:]))[0]:
-            delta = abs(V_test - V[i])
-            if delta < delta_min:
-                delta_min = delta
-                best_right_i = i
-            i += 1
+        # i = min_i
+        # best_right_i = i
+        # delta_min = abs(V[0] - min_V)
+        # while i <= np.where(V == max(V[min_i:]))[0]:
+        #     delta = abs(V_test - V[i])
+        #     if delta < delta_min:
+        #         delta_min = delta
+        #         best_right_i = i
+        #     i += 1
+
+        # Same thing I said above
+        min_guess_i = min_i
+        max_guess_i = np.where(V==max(V[min_i:]))[0][0]
+        i_guess_right = int((min_guess_i + max_guess_i) / 2)
+
+        index_finder_iteration = 0
+        while abs(V[i_guess_right] - V_test) > eps:
+            i_guess_right = int((min_guess_i + max_guess_i) / 2)
+            if V[i_guess_right] < V_test:
+                min_guess_i = i_guess_right
+            else:
+                max_guess_i = i_guess_right
+            if index_finder_iteration > 1000:
+                break
+            index_finder_iteration+=1
         
         # integrand
-        f = np.sqrt(2 * m * (V_test - V[best_left_i+1:best_right_i]))
+        f = np.sqrt(2 * m * (V_test - V[best_left_i+1:i_guess_right]))
         # integral
-        res = spi.trapz(f, x[best_left_i+1:best_right_i])
-
+        res = spi.trapz(f, x[best_left_i+1:i_guess_right])
+        # print(iteration, np.abs(res-target))
         # The binary search part
         if (res > target):
             V_test_max = V_test
@@ -112,15 +147,15 @@ for n in range(5):
     print("Energy: ", V_test)
     print("error: ", abs(res-target))
     E.append(V_test)
-    index_bounds.append((best_left_i, best_right_i))
+    index_bounds.append((best_left_i, i_guess_right))
 
 print()
 print()
 for i in range(len(E)-1):
     print(abs(E[i+1]-E[i]))
 
-l=20
-u=200
+l=1100
+u=4000
 
 plt.figure(figsize=(16/1.5,9/1.5))
 plt.title("Energy levels", fontsize=18)
@@ -131,4 +166,4 @@ for (n, (e, i)) in enumerate(zip(E,index_bounds)):
     plt.plot(x[i[0]:i[1]],[e] * (i[1]-i[0]), color='red')
 plt.legend()
 plt.savefig('levels.png')
-# plt.show()
+plt.show()
